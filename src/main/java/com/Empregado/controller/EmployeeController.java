652583dc928fca931cec60a2ddcf0a3ec9b96596
Bruno Romero <romero_bruno@msn.com>
2022-11-20 20:26:08 -0300

    Spring Security

--- src/main/java/com/Empregado/controller/EmployeeController.java
+++ src/main/java/com/Empregado/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Empregado.Repository.EmployeeRepository;
@@ -23,7 +25,8 @@ public class EmployeeController {
 	private EmployeeService employeeservice;
 	@Autowired
 	private DepartmentService departmentService;
-
+	
+	
 	@GetMapping("/empregados")
 	public List<Employee> listar() {
 		return empregadosrepository.findAll();
